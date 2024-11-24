21518be5ef9d4f17b26270eb4c38c0e6cee291db
Oscar <oscar.henriquez.g@usach.cl>
2024-11-24 17:58:42 -0300

    test first config

--- ms-config-server/src/main/java/com/mueblesstgo/ms_config_server/MsConfigServerApplication.java
+++ ms-config-server/src/main/java/com/mueblesstgo/ms_config_server/MsConfigServerApplication.java
@@ -2,11 +2,15 @@ package com.mueblesstgo.ms_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 public class MsConfigServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MsConfigServerApplication.class, args);
 	}
 
