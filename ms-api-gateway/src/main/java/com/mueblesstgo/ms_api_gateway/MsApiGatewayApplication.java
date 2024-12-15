497ad0440096600044e833706c3ff20d035a5b8b
Oscar <oscar.henriquez.g@usach.cl>
2024-12-15 20:15:47 -0300

    add: front y configs

--- ms-api-gateway/src/main/java/com/mueblesstgo/ms_api_gateway/MsApiGatewayApplication.java
+++ ms-api-gateway/src/main/java/com/mueblesstgo/ms_api_gateway/MsApiGatewayApplication.java
@@ -2,12 +2,17 @@ package com.mueblesstgo.ms_api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Habilita que el API Gateway sea cliente de Eureka
 public class MsApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsApiGatewayApplication.class, args);
 	}
-
 }
+
+
+
+
